9cad1a101a31894101a3eb58c82ffa409e7139e0
meowsome <tycarr01@gmail.com>
2023-06-22 02:51:05 -0400

    Use the official twitter v2 api to fetch latest status tweet

---
+++ _data/tweet.js
@@ -0,0 +1,29 @@
+const { TwitterApi } = require('twitter-api-v2');
+require('dotenv').config();
+
+module.exports = function() {
+    // Get Tweets from timeline
+    return new Promise(async function(res, reject) {
+        const userClient = new TwitterApi({
+            appKey: process.env.TWITTER_CONSUMER_KEY,
+            appSecret: process.env.TWITTER_CONSUMER_SECRET,
+            accessToken: process.env.TWITTER_ACCESS_TOKEN,
+            accessSecret: process.env.TWITTER_ACCESS_SECRET,
+        });
+
+        var latestTweet = (await userClient.currentUser()).status;
+
+        // Get and fix up Tweet content
+        var content = latestTweet['full_text'];
+        content = content.replace(/\s?https?:\/\/.*[\r\n]*\s?/g, ""); // Remove URLs
+        if (content.length > 25) content = content.replace(/^(.{25}[^\s]*).*/, "$1") + "..."; // Smart-truncate
+        if (content.length == 0) content = "media with no caption"; // If no text, say so
+
+        // Return Tweet content, image if exists, and url
+        res({
+            content: content,
+            image: (latestTweet['entities']['media']) ? latestTweet['entities']['media'][0]['media_url_https'] : "",
+            url: "https://twitter.com" + latestTweet['permalink']
+        });
+    });
+}
