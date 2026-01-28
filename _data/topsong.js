55b0fe92e95cda2373ee23d1249b8c29a2ea699d
meowsome <tycarr01@gmail.com>
2026-01-28 02:02:57 -0600

    Add extra search for artist picture when song picture not available

--- _data/topsong.js
+++ _data/topsong.js
@@ -17,25 +17,55 @@ module.exports = function() {
             console.log("No result from last fm");
             return;
         }
+        
+        var track = response.data.toptracks.track[0];
+        var params = {
+            method: "track.getinfo",
+            api_key: process.env.LASTFM,
+            format: "json"
+        };
+
+        if (track.mbid) {
+            params['mbid'] = track.mbid;
+        } else {
+            params['track'] = track.name;
+            params['artist'] = track.artist.name;
+        }
 
         return axios.get("https://ws.audioscrobbler.com/2.0", {
-            params: {
-                method: "track.getinfo",
-                mbid: response.data.toptracks.track[0].mbid,
-                api_key: process.env.LASTFM,
-                format: "json"
-            }
+            params: params
         }).then(function(response) {
             var song = response.data.track;
             
             // Remove artist name from title and truncate if too long
             if (song.name.length > 20) song.name = song.name.replace(/^(.{20}[^\s]*).*/, "$1") + "...";
 
-            return {
+            var responseData = {
                 song: song.name,
-                artwork: song.album.image[song.album.image.length - 1]['#text'],
                 url: song.url
             };
+
+            // Fetch artist picture if no album available
+            if ('album' in song) {
+                responseData['artwork'] = song.album.image[song.album.image.length - 1]['#text'];
+                return responseData;
+            } else {
+                return axios.get("https://ws.audioscrobbler.com/2.0", {
+                    params: {
+                        method: "artist.getinfo",
+                        api_key: process.env.LASTFM,
+                        format: "json",
+                        mbid: song.artist.mbid
+                    }
+                }).then(function(response) {
+                    responseData['artwork'] = response.data.artist.image[response.data.artist.image.length - 1]['#text'];
+                    return responseData;
+                }).catch(function(err) {
+                    console.log(err);
+                    console.log("Last fm artist.getinfo error");
+                    return;
+                });
+            }
         }, function(err) {
             console.log(err);
             console.log("Last fm track.getinfo error");
